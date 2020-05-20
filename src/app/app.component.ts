627790a049bf9fdd0fe68956b9a8dd14f3529f64
Pablo Oliva <p.oliva@memodo.de>
2020-05-20 13:18:17 +0200

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "mtms-root",
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+      <h1>Welcome to {{ title }}!</h1>
+      <span style="display: block">{{ title }} app is running!</span>
+    </div>
+    <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = "mtms";
+}
